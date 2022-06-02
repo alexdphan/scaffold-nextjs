cbc408c659cdce314e76ca6412ec096ad339177f
alexdphan <alexphan0515@gmail.com>
2022-06-01 23:21:31 -0400

    all fundamentals

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,15 +1,29 @@
-pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+// SPDX-License-Identifier: MIT
 
 // Primitive Data Types - 
 // Mappings - 
-// Structs
+// Structs - 
 // Modifiers -
-// Events
+// Events - put on the blockchain once and never retreive it
 // Inheritance -
 // Payable - 
-// Fallback
-// Just make it work for now
+// Fallback - Executed when function DNE, directly sends ETH (when stated as payable)
+   // Fallback executed when data isn't empty, Receive will be called when data is empty
+   // If fallback function only exists and doesn't have data, it will be executed
+   //    Which function is called, fallback() or receive()?
+
+   //         send Ether
+   //             |
+   //         msg.data is empty?
+   //             /       \
+   //           yes        no
+   //           /           \
+   //  receive() exists?     fallback()
+   //         /     \
+   //     yes        no
+   //     /           \
+   // receive()      fallback()
 
 import "hardhat/console.sol";
 
@@ -26,9 +40,10 @@ contract YourContract {
     string public name = "Alex";
     address public owner = 0x01c6fd9Ef45B69A24d3D59bEEbDc87c9c59dF2c7;
     string public purpose = "Building Unstoppable Apps!";
-    // event SetPurpose(address sender, string purpose);
-    // event SenderLogger(address);
-    // event ValueLogger(uint);
+    event SetPurpose(address sender, string purpose);
+    event SenderLogger(address);
+    event ValueLogger(uint);
+    event Log(string func, address sender, uint value, bytes data);
 
     mapping(address => uint) public balance;
 
@@ -37,7 +52,29 @@ contract YourContract {
     balance[owner] = 250000;
   }
 
+   // Example of a struct
+     struct Book { 
+      string title;
+      string author;
+      uint book_id;
+   }
+   // declaring a variable call book
+   // the type is specified as the struct named Book (above^)
+   Book book;
+
+   // function to input title, author, book id w/ memory
+   // set book = inputted book, which is Book
+   function setBook(string memory _title, string memory _author, uint _book_id) public {
+      book = Book(_title, _author, _book_id);
+   }
+  //  function to get the book info aftering the setBook function
+   function getBookId() public view returns (string memory, string memory, uint) {
+      return (book.title, book.author, book.book_id);
+   }
+
+
   // Transferring balances using the mapping balance
+  // using transfer method to send ETH
    function transfer(address to, uint amount) public {
     require(balance[msg.sender] >= amount, "Not enough.. You broke?");
     balance[msg.sender] -= amount;
@@ -67,13 +104,33 @@ contract YourContract {
   }
 
   // Changes the message of setPurpose if you pay 0.001 ether to the contract itself
+  // Also called emit to put on the blockchain once and never retreive it
   function setPurpose(string memory newPurpose) public payable{
     require(msg.value == 0.001 ether, "It's not enough ;/");
     // after effect happens here on before the action
     purpose = newPurpose;
     console.log(msg.sender, "set purpose to", purpose);
+    emit SetPurpose(msg.sender, purpose);
   }
 
   // withdraw function only if you are the owner
+  // takes the smart contract, turns it into an address, and gets the balance from it
+  // using call method to send ETH
+  function withdraw() public {
+    require(msg.sender == owner, "Not the owner.. oof");
+    (bool success, ) = owner.call{value: address(this).balance}("");
+    require(success, "failed ;(");
+  }
+
+  // receive() external payable — for empty calldata (and any value)
+  receive() external payable {
+    emit Log("receive", msg.sender, msg.value, "");
+  }
+
+
+  // Executed when function DNE, directly sends ETH (optionally payable)
+  fallback() external payable {
+    emit Log("fallback", msg.sender, msg.value, msg.data);
+  }
 }
 
